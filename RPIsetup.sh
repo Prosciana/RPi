cc8c520e3397cc8cdf4b5ebadf8d619eb801746d
Carlos <carlos@prosciana.com>
2014-09-26 10:40:38 -0500

    1

---
+++ RPIsetup.sh
@@ -0,0 +1,35 @@
+# Rasp Pi Setup
+# in host
+# format
+df -h
+# unmount
+sudo dd bs=1m if=[DISK IMAGE NAME] of=[FILESYSTEM]
+
+sudo nano /etc/network/interfaces
+# Insert 
+# auto lo
+
+# iface lo inet loopback
+# iface eth0 inet dhcp
+
+# allow-hotplug wlan0
+# auto wlan0
+
+
+# iface wlan0 inet dhcp
+#         wpa-ssid "INFINITUMCE8BEB"
+#         wpa-psk "37DB81D554"
+sudo halt
+# Connect WIFI 
+# Power on
+# DevApps Setup
+
+# Arduino
+sudo apt-get update
+sudo apt-get install arduino -y
+sudo apt-get install python-dev python-setuptools -y
+git clone git://github.com/amperka/ino.git
+cd ino
+sudo python setup.py install
+sudo apt-get install picocom -y
+cd
